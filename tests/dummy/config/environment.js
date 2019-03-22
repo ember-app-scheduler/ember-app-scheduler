290222fc85ed1d37dc7d19b58b2142f0d2dd7ff8
Steve Calvert <scalvert@linkedin.com>
2019-03-22 11:30:55 -0700

    Fixing config for docs

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,11 +44,5 @@ module.exports = function(environment) {
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
-    // Allow ember-cli-addon-docs to update the rootURL in compiled assets
-    ENV.rootURL = 'ADDON_DOCS_ROOT_URL';
-    // here you can enable a production-specific feature
-  }
-
   return ENV;
 };
