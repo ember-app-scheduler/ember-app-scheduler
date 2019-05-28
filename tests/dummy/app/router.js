674d0914e4e5846b4ef497dced34c98c37c307b9
Steve Calvert <scalvert@linkedin.com>
2019-05-28 08:36:02 -0700

    Removing ember-cli-addon-docs to attempt to track down segfault in Travis

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,19 +1,16 @@
-import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
-import RouterScroll from 'ember-router-scroll';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = AddonDocsRouter.extend(RouterScroll, {
+const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
 });
 
 Router.map(function() {
-  docsRoute(this, function() {
-    this.route('how-it-works');
-    this.route('when-route-painted');
-    this.route('when-route-idle');
-    this.route('testing');
-  });
+  this.route('how-it-works');
+  this.route('when-route-painted');
+  this.route('when-route-idle');
+  this.route('testing');
   this.route('first-paint');
   this.route('content-paint');
   this.route('aborted-paint');
