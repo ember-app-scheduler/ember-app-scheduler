1db1f58a6fbb51390d2abdcc09995f4da2bc212a
Steve Calvert <scalvert@linkedin.com>
2019-11-20 09:03:06 -0800

    Adding test for page transition. Adding Firefox to browser test matrix

--- testem.js
+++ testem.js
@@ -2,7 +2,7 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_dev: ['Chrome', 'Firefox'],
   browser_args: {
     Chrome: {
       ci: [
