0056f660e306ba1d8f8641e69ad3c97a6d29dcd6
Steve Calvert <scalvert@linkedin.com>
2019-11-20 09:05:00 -0800

    Adding Firefox to CI test matrix

--- testem.js
+++ testem.js
@@ -1,8 +1,8 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome', 'Firefox'],
+  launch_in_ci: ['Chrome', 'Firefox'],
+  launch_in_dev: ['Chrome'],
   browser_args: {
     Chrome: {
       ci: [
@@ -15,6 +15,9 @@ module.exports = {
         '--remote-debugging-port=0',
         '--window-size=1440,900',
       ].filter(Boolean),
+    },
+    Firefox: {
+      ci: ['-headless'],
     },
   },
 };
