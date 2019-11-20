c091b7e63b247d489d02aecfd04ab62e7012beaa
Steve Calvert <scalvert@linkedin.com>
2019-11-20 10:32:29 -0800

    Update addon/scheduler.ts
    Co-Authored-By: Robert Jackson <me@rwjblue.com>

--- addon/scheduler.ts
+++ addon/scheduler.ts
@@ -94,7 +94,7 @@ export function didTransition(): Promise<any> {
  */
 export function whenRoutePainted(): Promise<any> {
   deprecate(
-    'The `whenRoutePained` function is deprecated. Please use `whenRouteIdle` to defer work.',
+    'The `whenRoutePained` function is deprecated. Please use `whenRouteIdle` instead.',
     false,
     {
       id: 'ember-app-scheduler.whenRoutePainted',
