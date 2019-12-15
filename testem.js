5118c80cb0bc0f911e1b523c22d48cf1dc7b7260
Steve Calvert <scalvert@linkedin.com>
2019-12-15 09:51:33 -0800

    task(CI): Converting to github actions

--- testem.js
+++ testem.js
@@ -1,8 +1,9 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome', 'Firefox'],
+  launch_in_ci: process.env.TEST_IE ? ['IE'] : ['Chrome', 'Firefox'],
   launch_in_dev: ['Chrome'],
+  browser_start_timeout: 120,
   browser_args: {
     Chrome: {
       ci: [
