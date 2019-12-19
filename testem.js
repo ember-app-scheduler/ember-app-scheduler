8e90be6692ae68ec46ffb401fd72562d169a6497
Steve Calvert <scalvert@linkedin.com>
2019-12-19 09:25:01 -0800

    Remove IE config

--- testem.js
+++ testem.js
@@ -1,7 +1,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: process.env.TEST_IE ? ['IE'] : ['Chrome', 'Firefox'],
+  launch_in_ci: ['Chrome', 'Firefox'],
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
