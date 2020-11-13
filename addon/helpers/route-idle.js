8c1e38116134749eba1feb591ba8fe3a56d2b1c8
Nicholas Eidler <neidler@linkedin.com>
2020-11-13 11:11:01 -0800

    Recompute RouteIdle helper state on route change

--- addon/helpers/route-idle.js
+++ addon/helpers/route-idle.js
@@ -1,9 +1,22 @@
 import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { whenRouteIdle } from '../scheduler';
 
 export default class RouteIdle extends Helper {
+  @service router;
+
   @tracked isIdle = false;
+
+  constructor() {
+    super(...arguments);
+
+    this.router.on('routeWillChange', () => {
+      if (!this.isDestroying) {
+        this.isIdle = false;
+      }
+    });
+  }
 
   compute() {
     if (this.isIdle) {
@@ -11,7 +24,9 @@ export default class RouteIdle extends Helper {
     }
 
     whenRouteIdle().then(() => {
-      this.isIdle = true;
+      if (!this.isDestroying) {
+        this.isIdle = true;
+      }
     });
 
     return false;
