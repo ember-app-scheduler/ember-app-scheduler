746e808329bcc36382df3a287864a2df060076fd
Sergey Astapov <sergeastapov@gmail.com>
2021-11-03 23:40:29 -0400

    Run ember-cli-update to v3.28.3 and enable embroider scenarios

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,32 +10,29 @@ module.exports = {
       legacyDecorators: true,
     },
   },
-  plugins: ['ember', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
   env: {
     browser: true,
   },
-  rules: {
-    'ember/no-jquery': 'error',
-  },
+  rules: {},
   overrides: [
     // node files
     {
       files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js',
-      ],
-      excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**',
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
       ],
       parserOptions: {
         sourceType: 'script',
@@ -45,13 +42,7 @@ module.exports = {
         node: true,
       },
       plugins: ['node'],
-      rules: Object.assign(
-        {},
-        require('eslint-plugin-node').configs.recommended.rules,
-        {
-          // add your custom rules and overrides for node files here
-        }
-      ),
+      extends: ['plugin:node/recommended'],
     },
     {
       // typescript files
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,13 +1,14 @@
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
 import { setup } from 'qunit-dom';
-import Application from '../app';
-import config from '../config/environment';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
 
-setApplication(Application.create(config.APP));
 start({
   setupTestIsolationValidation: true,
 });
