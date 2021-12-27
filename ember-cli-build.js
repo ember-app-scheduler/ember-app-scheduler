19dfcf101dac92c8cc6cd2565d4f0c4e2be95b06
Sergey Astapov <SergeAstapov@gmail.com>
2021-12-27 13:42:11 -0500

    Run ember-cli-update to v3.28.3 and enable embroider-safe scenario (#1169)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,7 @@
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
   });
@@ -14,5 +14,24 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  return app.toTree();
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    // Support tests with dynamic component registration in Embroider.
+    // See https://github.com/embroider-build/embroider/issues/522
+    packageRules: [
+      {
+        package: 'dummy',
+        components: {
+          '{{deferred-render}}': {
+            safeToIgnore: true,
+          },
+        },
+      },
+    ],
+  });
 };
